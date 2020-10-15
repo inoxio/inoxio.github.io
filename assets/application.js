365479640e923e2b66b231b46348ea9090bad788
Torsten Mangner <tmangner@inoxio.de>
2020-10-15 16:40:43 +0200

    Play around with colors and dummy images

--- assets/application.js
+++ assets/application.js
@@ -5,27 +5,27 @@ Array.prototype.random = function () {
 const claims = [
     {
         heading: '... wenn man zum Tiefseetauchen lediglich einen Schnorchel bringt.',
-        image: 'img/snorkel.jpg',
+        image: 'img/example4.png',
     },
     {
         heading: '... wenn man für alles immer nur einen Hammer benutzt, obwohl die Werkzeugkiste prall gefüllt ist',
-        image: 'img/toolbox.jpg',
+        image: 'img/example3.png',
     },
-    {
-        heading: '... wenn der Mechaniker in der Werkstatt bei einem Motorschaden das Auto nur von außen begutachtet.',
-        image: 'img/mechanic.jpg',
-    },
-    {
-        heading: '... wenn man ein Sieben-Gänge-Menü nur mit einer Gabel isst.',
-        image: 'img/cutlery.jpg',
-    },
+    // {
+    //     heading: '... wenn der Mechaniker in der Werkstatt bei einem Motorschaden das Auto nur von außen begutachtet.',
+    //     image: 'img/mechanic.jpg',
+    // },
+    // {
+    //     heading: '... wenn man ein Sieben-Gänge-Menü nur mit einer Gabel isst.',
+    //     image: 'img/cutlery.jpg',
+    // },
     {
         heading: '... wenn man für einen 100m Sprint die Laufschuhe erst nach 90m anzieht.',
-        image: 'img/running.jpg',
+        image: 'img/example2.png',
     },
     {
         heading: '... wenn man für Double Chocolate Cookies, ausschließlich im Nachhinein Schokostreusel auf die Kekse streut.',
-        image: 'img/cookie.jpg',
+        image: 'img/example1.png',
     },
 ];
 
@@ -35,5 +35,5 @@ function showClaims() {
     const claim = claims.random();
     document.getElementById("claimtext").textContent = claim.heading;
     document.getElementById("claimimage").src = `/assets/${claim.image}`;
-    setTimeout(showClaims, 5000);
+    // setTimeout(showClaims, 5000);
 }
